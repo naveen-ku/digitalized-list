65a21c76e702239907d27fdcf67269c76ffca883
naveen-ku <naveen2951996@gmail.com>
2020-04-12 19:41:53 +0530

    Completely setting up webpack

--- src/app.js
+++ src/app.js
@@ -1,1 +1,7 @@
-console.log("Webpack is running")
+import validator from "validator";
+import React from "react";
+import ReactDOM from "react-dom";
+
+const app =<p>hello</p>;
+ReactDOM.render(app, document.getElementById("root"));
+
