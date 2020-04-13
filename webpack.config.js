59eb46afbe0b0ad24aff714d7f36eefabe028b8f
naveen-ku <naveen2951996@gmail.com>
2020-04-13 16:42:38 +0530

    setting up webpack nad scss

--- webpack.config.js
+++ webpack.config.js
@@ -13,10 +13,14 @@ module.exports = {
         test: /\.js$/,
         exclude: /node_modules/,
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
     ],
   },
-  devtool:'cheap-module-eval-source-map',
-  devServer:{
-    contentBase:path.join(__dirname, "public")
-  }
+  devtool: "cheap-module-eval-source-map",
+  devServer: {
+    contentBase: path.join(__dirname, "public"),
+  },
 };
