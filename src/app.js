7de39c81af0281c36a510f60eb2c9814e4dcb505
naveen-ku <naveen2951996@gmail.com>
2020-04-13 17:12:37 +0530

    Adding styles to header

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import ToDoMixApp from './components/ToDoMixApp'
+import './styles/styles.scss';
 
   
   ReactDOM.render(<ToDoMixApp />, document.getElementById("root"));
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,31 +1,30 @@
-import React from 'react';
+import React from "react";
 
+// class Header extends React.Component {
+//   render() {
+//     // console.log(this.props);
+//     return (
+//       <div>
+//         <h1>{this.props.title}</h1>
+//         <h2>{this.props.subtitle}</h2>
+//       </div>
+//     );
+//   }
+// }
 
-  // class Header extends React.Component {
-  //   render() {
-  //     // console.log(this.props);
-  //     return (
-  //       <div>
-  //         <h1>{this.props.title}</h1>
-  //         <h2>{this.props.subtitle}</h2>
-  //       </div>
-  //     );
-  //   }
-  // }
-  
-  //Used Stateless Component because here we don't need to change the state
-  const Header = (props) => {
-    return (
-      <div>
-        <h1>{props.title}</h1>
-        {props.subtitle && <h2>{props.subtitle}</h2>}
-      </div>
-    );
-  };
-  
-  //Setting up of default props value
-  Header.defaultProps = {
-    title: "ToDo Mix App",
-  };
-  
-  export default Header;
+//Used Stateless Component because here we don't need to change the state
+const Header = (props) => {
+  return (
+    <div className="header">
+      <h1 className="header__title">{props.title}</h1>
+      {props.subtitle && <h2 className="header__subtitle">{props.subtitle}</h2>}
+    </div>
+  );
+};
+
+//Setting up of default props value
+Header.defaultProps = {
+  title: "ToDo Mix App",
+};
+
+export default Header;
